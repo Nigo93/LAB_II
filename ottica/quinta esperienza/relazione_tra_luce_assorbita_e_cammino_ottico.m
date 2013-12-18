b578913879605f787168026ad85dec9044bc39dc
Nigo93 <nicola.trevisson@gmail.com>
2013-12-18 17:10:10 +0100

    update
    asd

--- ottica/quinta esperienza/relazione_tra_luce_assorbita_e_cammino_ottico.m
+++ ottica/quinta esperienza/relazione_tra_luce_assorbita_e_cammino_ottico.m
@@ -1,16 +1,18 @@
+% lunghezza del cammino ottico
 l=[20; 25; 30; 35; 40; 50; 60; 70];
-I=[55.9; 44.3; 37.4; 25; 23.5; 19.8; 14.8; 10.3]+4.3;
-c=0.125 %inventato D:
+%offset dello strumento
+off=4.3;
+% Intensità luminosa
+I=[55.9+off; 44.3+off; 37.4+off; 25+off; 23.5+off; 19.8; 14.8; 10.3];
+c=0.132  %inventato D:
 
-
+%calcolo dei parametri x e y per Y=a+bx
 y=log(I);
 X=c.*l;
+%errore sui dati sperimentali
 dy=y.*0.01;
 dx=X.*0.01;
-%linReg(x,y);
+% calcolo della regressione lineare
 [A,B,d_a,d_b] = reglin( X,y,dy )
-plot(l,y)
-hold on
-cacca=[0:0.01:100]
-grid on
-plot(cacca,B*cacca+A)
+io=exp(A)
+
